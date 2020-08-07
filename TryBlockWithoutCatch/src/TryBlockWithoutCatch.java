f29fd04bc7e6146154bcc48d71f2163d92e2f06c
abhilashj09 <abhij11909@gmail.com>
2020-08-07 22:23:39 +0530

    Without Catch

---
+++ TryBlockWithoutCatch/src/TryBlockWithoutCatch.java
@@ -0,0 +1,14 @@
+
+public class TryBlockWithoutCatch {
+
+	public static void main(String[] args) {
+		
+		try {
+	         System.out.println("Try Block");
+	      } finally {
+	         System.out.println("Finally Block");
+	      }
+
+	}
+
+}
