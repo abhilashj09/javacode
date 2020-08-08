18cb34481c5b6fa8b8aeeaec778da596d35321ba
abhilashj09 <abhij11909@gmail.com>
2020-08-08 15:01:58 +0530

    IntToChar

---
+++ IntToCharAssigment/src/IntToChar.java
@@ -0,0 +1,18 @@
+
+public class IntToChar {
+
+	public static void main(String[] args) {
+		
+System.out.println(covertIntToCharacter(70));
+	}
+
+	
+	public static Character covertIntToCharacter(int x )
+	{
+		int value = x;
+		Character myChar = (char) x;
+		return myChar;
+		
+		
+	}
+}
